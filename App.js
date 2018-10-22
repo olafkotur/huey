e01e2c2f9190c108dcb03135620d663f5d1e2993
olafkotur <olafkotur97@gmail.com>
2018-10-22 15:00:41 +0100

    Initial push

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import styles from "./Styles";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello</Text>
+      </View>
+    );
+  }
+}
---
+++ Styles.js
@@ -0,0 +1,25 @@
+import React, { StyleSheet, Platform, Dimensions } from 'react-native';
+
+// Device dimension declaration
+var deviceHeight = Dimensions.get('window').height;
+var deviceWidth = Dimensions.get('window').width;
+
+export default StyleSheet.create({
+
+
+  /* Table of Contents
+    1. Global
+  */
+
+  // 1. Global
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 50,
+  }
+});
