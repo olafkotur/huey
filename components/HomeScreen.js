98aed1cea3b139cb82daff0df784e315fdccd784
George Park <g.park1@lancaster.ac.uk>
2019-01-21 15:58:02 +0000

    Removed statusbar

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image,TouchableOpacity, } from 'react-native';
+import { StyleSheet, Text, View, Image,TouchableOpacity,  StatusBar } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import NativeCamera from './NativeCamera.js';
@@ -21,6 +21,7 @@ export default class HomeScreen extends React.Component {
 	render() {
 		return (
 			<View style = {styles.container}>
+				<StatusBar hidden = {true} />
 			
 				<TouchableOpacity
 					style = {styles.folderButton}
