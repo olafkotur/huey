0b3f92cec62c9e19d15f433374fc90ed7c055b3c
olafkotur <olafkotur97@gmail.com>
2019-02-15 17:53:55 +0000

    Playing audio and being able to stop it

--- components/AudioList.js
+++ components/AudioList.js
@@ -7,92 +7,92 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class AudioList extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.sound = null;
-	}
+    constructor(props) {
+        super(props);
+        this.sound = null;
+    }
 
-	state = {
-		refreshing: false,
-		isPlaying: false,
-		audioData: [],
-		playIcon: 'play'
-	}
+    state = {
+        refreshing: false,
+        isPlaying: false,
+        audioData: [],
+        playIcon: 'play'
+    }
 
-	componentDidMount = async () => {
-		this.fetchData();
-	}
+    componentDidMount = async () => {
+        this.fetchData();
+    }
 
-	// Fetches audio data from firebase
-	fetchData = async () => {
-		this.setState({refreshing: true});
-		Handler = new FileHandler();
-		await Handler.getMedia('audio').then((data) => this.setState({audioData: data}));
-		this.setState({refreshing: false});
-	}
+    // Fetches audio data from firebase
+    fetchData = async () => {
+        this.setState({refreshing: true});
+        Handler = new FileHandler();
+        await Handler.getMedia('audio').then((data) => this.setState({audioData: data}));
+        this.setState({refreshing: false});
+    }
 
-	// Plays back selected audio
-	handlePlayback = async (url) => {
-		if (this.state.isPlaying) {
-			console.log('Stopping playback');
-			this.setState({isPlaying: false, playIcon: 'play'});
-			// this.sound.stopAsync();
-		}
-		else {
-			console.log('Attempting to playback');
-			this.setState({isPlaying: true, playIcon: 'pause'});
+    // Plays back selected audio
+    handlePlayback = async (url) => {
+        // Stop audio if already playing
+        if (this.state.isPlaying) {
+            this.setState({isPlaying: false, playIcon: 'play'});
+            await this.sound.stopAsync();
+            await this.sound.unloadAsync();
+        }
+        
+        // Play audio if not already playing 
+        else {
+            this.setState({isPlaying: true, playIcon: 'pause'});
 
-			// const sound = new Audio.Sound();
-			// try {
-			// 	await sound.loadAsync({uri: url});
-			// 	this.sound = sound;
-			// 	await sound.playAsync();
-			// 	console.log('Sound is playing')
-			// } catch (error) {
-			// 	console.log(error.message);
-			// }
-		}
-	}
+            const sound = new Audio.Sound();
+            try {
+                await sound.loadAsync({uri: url});
+                this.sound = sound;
+                await sound.playAsync();
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+    }
 
-	// Renders each audio file
-	renderItem = (item) => {
-		// Format date DD:MM:YYYY, hh:mm,ss
-		const fileName = item.url.split('audio%2F').pop().split('?')[0].split('.')[0];
-		var options = { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'};
-		let date = new Date(parseInt(fileName));
-		//date = new Intl.DateTimeFormat('en-GB', options).format(date);
+    // Renders each audio file
+    renderItem = (item) => {
+        // Format date DD:MM:YYYY, hh:mm,ss
+        const fileName = item.url.split('audio%2F').pop().split('?')[0].split('.')[0];
+        var options = { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'};
+        let date = new Date(parseInt(fileName));
+        date = new Intl.DateTimeFormat('en-GB', options).format(date);
 
-		return (
-				<View 
-					style = {styles.audioItemContainer}>
-					<Text style = {styles.audioItemTitle}>{date.toString()}</Text>
+        return (
+                <View 
+                    style = {styles.audioItemContainer}>
+                    <Text style = {styles.audioItemTitle}>{date}</Text>
 
-					<Text>00:00:00/00:02:37</Text>
+                    <Text>00:00:00/00:02:37</Text>
 
-					<TouchableOpacity 
-						style={styles.audioPlayButton}
-						onPress = {() => this.handlePlayback(item.url)}>
-						<Icon name={this.state.playIcon} style = {styles.audioPlayIcon} size = {40}/>
-					</TouchableOpacity>
+                    <TouchableOpacity 
+                        style={styles.audioPlayButton}
+                        onPress = {() => this.handlePlayback(item.url)}>
+                        <Icon name={this.state.playIcon} style = {styles.audioPlayIcon} size = {40}/>
+                    </TouchableOpacity>
 
-
-				</View>
-		);
-	}
+                </View>
+        );
+    }
 
 
-	render() {
-		return (
-			<View style = {styles.galleryTabViewContainer}>	
-				<FlatList
-					data = {this.state.audioData}
-					extraData = {this.state}
-					keyExtractor = {(item, index) => index.toString()}
-					renderItem = {({item}) => this.renderItem(item)}
-					refreshing = {this.state.refreshing}
-					onRefresh = {() => this.fetchData()}>
-				</FlatList>
-			</View>
-		);
-	}
+    render() {
+        return (
+            <View style = {styles.galleryTabViewContainer}>	
+                <FlatList
+                    data = {this.state.audioData}
+                    extraData = {this.state}
+                    keyExtractor = {(item, index) => index.toString()}
+                    renderItem = {({item}) => this.renderItem(item)}
+                    refreshing = {this.state.refreshing}
+                    onRefresh = {() => this.fetchData()}>
+                </FlatList>
+            </View>
+        );
+    }
 }
--- components/ImageList.js
+++ components/ImageList.js
@@ -8,50 +8,50 @@ import FileHandler from './FileHandler';
 
 class ImageList extends React.Component {
 
-	state = {
-		refreshing: false,
-		mediaData: [],
-	}
+    state = {
+        refreshing: false,
+        mediaData: [],
+    }
 
-	componentDidMount = async () => {
-		this.fetchData();
-	}
+    componentDidMount = async () => {
+        this.fetchData();
+    }
 
-	fetchData = async () => {
-		this.setState({refreshing: true});
-		Handler = new FileHandler();
-		await Handler.getMedia('media').then((data) => this.setState({mediaData: data}));
-		this.setState({refreshing: false});
-	}
+    fetchData = async () => {
+        this.setState({refreshing: true});
+        Handler = new FileHandler();
+        await Handler.getMedia('media').then((data) => this.setState({mediaData: data}));
+        this.setState({refreshing: false});
+    }
 
-	renderItem = (item) => {
-		const fileName = item.url.split('media%2F').pop().split('?')[0];
-		const fileType = fileName.includes('.png') ? 'photo' : 'video';
+    renderItem = (item) => {
+        const fileName = item.url.split('media%2F').pop().split('?')[0];
+        const fileType = fileName.includes('.png') ? 'photo' : 'video';
 
-		return (
-			<TouchableOpacity
-				onPress = {() => this.props.navigation.navigate('FocusedImage', {uri: item.url, fileType: fileType})} >
-				<GalleryImage uri = {item.url} fileType = {fileType} />
-			</TouchableOpacity>
-		);
-	}
+        return (
+            <TouchableOpacity
+                onPress = {() => this.props.navigation.navigate('FocusedImage', {uri: item.url, fileType: fileType})} >
+                <GalleryImage uri = {item.url} fileType = {fileType} />
+            </TouchableOpacity>
+        );
+    }
 
-	render() {
-		return (
-			<View style = {styles.galleryTabViewContainer}>				
-	  			<FlatList
-					data = {this.state.mediaData}
-					extraData = {this.state}
-					horiztonal = {false}
-					numColumns = {3}
-					keyExtractor = {(item, index) => index.toString()}
-					renderItem = {({item}) => this.renderItem(item)}
-					refreshing = {this.state.refreshing}
-					onRefresh = {() => this.fetchData()} >
-				</FlatList>
-			</View>
-		);
-	}
+    render() {
+        return (
+            <View style = {styles.galleryTabViewContainer}>				
+                  <FlatList
+                    data = {this.state.mediaData}
+                    extraData = {this.state}
+                    horiztonal = {false}
+                    numColumns = {3}
+                    keyExtractor = {(item, index) => index.toString()}
+                    renderItem = {({item}) => this.renderItem(item)}
+                    refreshing = {this.state.refreshing}
+                    onRefresh = {() => this.fetchData()} >
+                </FlatList>
+            </View>
+        );
+    }
 }
 
 export default withNavigation(ImageList);
