82b5b637707aac0dba0ec2e49a3929341736efb2
olafkotur <olafkotur97@gmail.com>
2019-02-15 11:32:31 +0000

    Not complete but ready for front end

--- components/AudioList.js
+++ components/AudioList.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Text, View, TouchableOpacity, FlatList } from 'react-native';
-
+import { Audio } from 'expo';
 import styles from "../Styles";
-import GalleryImage from './GalleryImage';
 import FileHandler from './FileHandler';
 
 export default class AudioList extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.sound = null;
+	}
+
 	state = {
 		refreshing: false,
+		isPlaying: false,
 		audioData: []
 	}
 
@@ -25,22 +30,47 @@ export default class AudioList extends React.Component {
 	}
 
 	// Plays back selected audio
-	handlePlayback = () => {
-		console.log('Attempting to playback');
+	handlePlayback = async (url) => {
+		if (this.state.isPlaying) {
+			console.log('Stopping playback');
+			this.setState({isPlaying: false});
+			this.sound.stopAsync();
+		}
+		else {
+			console.log('Attempting to playback');
+			this.setState({isPlaying: true});
+
+			const sound = new Audio.Sound();
+			try {
+				await sound.loadAsync({uri: url});
+				this.sound = sound;
+				await sound.playAsync();
+				console.log('Sound is playing')
+			} catch (error) {
+				console.log(error.message);
+			}
+		}
 	}
 
 	// Renders each audio file
 	renderItem = (item) => {
+		// Format date DD:MM:YYYY, hh:mm,ss
 		const fileName = item.url.split('audio%2F').pop().split('?')[0].split('.')[0];
 		var options = { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'};
 		let date = new Date(parseInt(fileName));
 		date = new Intl.DateTimeFormat('en-GB', options).format(date);
+
 		return (
-			<TouchableOpacity
-				style = {styles.audioItemContainer}
-				onPress = {() => this.handlePlayback(item.url)}>
+			<View
+				style = {styles.audioItemContainer}>
 				<Text>{date}</Text>
-			</TouchableOpacity>
+
+				<TouchableOpacity 
+					onPress = {() => this.handlePlayback(item.url)}>
+					<Text>Play/Pause</Text>
+				</TouchableOpacity>
+
+			</View>
 		);
 	}
 
